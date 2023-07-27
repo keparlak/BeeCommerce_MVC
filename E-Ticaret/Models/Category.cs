488e36baa8745883babf0026a5996769c38bb9a0
EmrCvk <cevikemir0@hotmail.com>
2023-07-27 16:51:05 +0300

    v0.1.2

--- E-Ticaret/Models/Category.cs
+++ E-Ticaret/Models/Category.cs
@@ -5,8 +5,9 @@ namespace E_Ticaret.Models
     public class Category
     {
         [Key]
-        public int Id { get; set; }
-        [Required]
+        public int Id{ get; set; }
+
+        [Required] 
         public string Name { get; set; }
     }
 }
