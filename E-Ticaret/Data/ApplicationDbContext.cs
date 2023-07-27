70d857e93513dba507ec9823226ad5611b02811e
EmrCvk <cevikemir0@hotmail.com>
2023-07-27 13:45:03 +0300

    initial commit

---
+++ E-Ticaret/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Ticaret.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Ticaret.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
