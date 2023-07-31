c1a80e9c9a3fdfacf804347a6ccf1cbe327bbeeb
EmrCvk <cevikemir0@hotmail.com>
2023-07-31 13:26:57 +0300

    v0.1a

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı kısmı boş bırakılamaz.");
+            RuleFor(x=>x.CategoryDescription).NotEmpty().WithMessage("Açıklama kısmı boş bırakılamaz.");
+            RuleFor(x => x.CategoryName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girin.");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20den fazla karakter girmeyin.");
+
+
+        }
+    }
+}
