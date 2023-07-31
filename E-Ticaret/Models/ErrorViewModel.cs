0593b39c2502f64603fb5d5e2ab9150a855e5ef9
EmrCvk <cevikemir0@hotmail.com>
2023-07-31 13:09:44 +0300

    initial commit 5

--- E-Ticaret/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace E_Ticaret.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
